9093b4dbd71ecd358142d1a98df8665c2d4f1695
Vitalii <kv111nt@gmail.com>
2019-12-18 19:16:23 +0200

    Add Mops Comments

--- src/store/company/actions.ts
+++ src/store/company/actions.ts
@@ -36,6 +36,7 @@ export function createCompany(newCompany : Company) {
         .then(res => {
             dispatch(setCompanies(res.data));
             return res;
+            //Mops
         });
     }
 }
